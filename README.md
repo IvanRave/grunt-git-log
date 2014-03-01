0525c67c38e8ba0da93657f6b145eb95327d936c
Ivan <ivanich86@gmail.com>
2014-03-01 04:48:25 +0200

    feat(task): new logic and format

--- README.md
+++ README.md
@@ -10,6 +10,16 @@ A [grunt](https://github.com/cowboy/grunt) plugin for outputting commit messages
 
 Outputs git commit messages
 
+Example:
+
+gitlog: {
+          options: {
+            dest: 'doc/month.log',
+            afterDate: new Date(2014, 1, 1),
+            beforeDate: new Date(2014, 2, 1)
+          }
+        },
+
 ## License
 Copyright (c) 2012 Jeremy Jannotta
 
--- tasks/git-log.js
+++ tasks/git-log.js
@@ -6,23 +6,28 @@ module.exports = function (grunt) {
 
 		this.requiresConfig('gitlog');
 
+		var separator = '==--==';
+		// format:"%B%n"
+		// format:%s#%b
+		var logFormat = '%s\n%b' + separator;
+
 		var config = grunt.config.get('gitlog'),
 		done = this.async(),
 		afterDate,
 		beforeDate,
 		dest,
-		gitArgs = ['log', '--pretty=format:"%B%n"', '--date-order', '--no-merges'];
+		gitArgs = ['log', '--pretty=format:' + logFormat, '--date-order', '--no-merges'];
 
-    if (config) {
-      var opts = config.options;
-      if (opts){
-        afterDate = opts.afterDate || new Date(Date.now() - (1000 * 60 * 60 * 24)),
-        beforeDate = opts.beforeDate || new Date(),
-        if (opts.dest){
-          dest = grunt.template.process(opts.dest);
-        }
-      }
-    }
+		if (config) {
+			var opts = config.options;
+			if (opts) {
+				afterDate = opts.afterDate || new Date(Date.now() - (1000 * 60 * 60 * 24));
+				beforeDate = opts.beforeDate || new Date();
+				if (opts.dest) {
+					dest = grunt.template.process(opts.dest);
+				}
+			}
+		}
 
 		gitArgs.push('--after="' + dateFormat(afterDate, "isoDateTime") + '"');
 
@@ -40,7 +45,36 @@ module.exports = function (grunt) {
 			}
 
 			if (dest !== undefined) {
-				grunt.file.write(dest, result);
+				var listOfBodyOfCommit = result.toString().split(separator);
+
+				var listOfTabBody = [];
+
+				var maxTimeLength = 'time: 12h:34'.length;
+
+				listOfBodyOfCommit.forEach(function (commitItem) {
+					var timeIndex = commitItem.indexOf('time:');
+
+					var timeStr;
+					var mainStr;
+
+					if (timeIndex >= 0) {
+						timeStr = commitItem.substr(timeIndex, maxTimeLength);
+						timeStr = timeStr.replace('time:', '').replace('h', '').trim();
+						// Remove this part from the item (the time part - last part)
+						mainStr = commitItem.substr(0, timeIndex);
+					} else {
+						mainStr = commitItem;
+					}
+
+					// Remove enters and trim the string
+					mainStr = mainStr.replace(/\n/g, ' ').trim();
+
+					if (mainStr) {
+						listOfTabBody.push(mainStr + '\t' + (timeStr || 0));
+					}
+				});
+
+				grunt.file.write(dest, listOfTabBody.join('\n'));
 				grunt.log.ok('Git log written to ' + dest);
 
 			} else {
